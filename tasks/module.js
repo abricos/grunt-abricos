fc9a224790a95bf530fdb7e6f0d3405f8c793cf7
Alexander Kuzmin <roosit@abricos.org>
2014-12-27 22:21:57 +0300

    builder of module

--- tasks/module.js
+++ tasks/module.js
@@ -12,7 +12,6 @@
 var Module = require('../lib/module');
 
 module.exports = function(grunt){
-
     grunt.registerMultiTask('abmodule', 'Build Abricos Module', function(){
 
         var config = require('../lib/config.js').instance();
@@ -31,69 +30,6 @@ module.exports = function(grunt){
 
         abModule.build(function(err){
             done(err);
-        });
-
-        return; ///////////////////
-
-
-        var projectDir = options.directory;
-
-        var isModule = checkAbricosModule(projectDir);
-        if (!isModule){
-            grunt.log.warn('Abricos module "' + projectDir + '" not found.');
-            return;
-        }
-
-        var srcDir = path.resolve(projectDir, 'src');
-        var buildDir = path.resolve(projectDir, options.buildDir);
-
-        if (options.cleanBuildDir && fs.existsSync(buildDir)){
-            // Clean build directory
-            fse.removeSync(buildDir);
-        }
-
-        var done = this.async();
-        var stack = [];
-
-        // JS Component
-        stack.push(function(stackCallback){
-            jscomp(grunt, options, stackCallback);
-        });
-
-        // LESS
-        stack.push(function(stackCallback){
-            var lessSrcDir = path.join(srcDir, 'less');
-            var lessDestDir = path.join(buildDir, 'css');
-            less(grunt, lessSrcDir, lessDestDir, options, stackCallback);
-        });
-
-        // copy
-        stack.push(function(stackCallback){
-            fse.copySync(srcDir, buildDir);
-            stackCallback(null);
-        });
-
-        // Delete work files
-        stack.push(function(stackCallback){
-
-            var lessDestDir = path.join(buildDir, 'less');
-            fse.removeSync(lessDestDir);
-
-            var globFiles = glob.sync(path.join(buildDir, 'js/*.less'));
-            for (var i = 0; i < globFiles.length; i++){
-                var file = globFiles[i];
-                fse.removeSync(file);
-            }
-
-            stackCallback(null);
-        });
-
-        async.series(stack, function(err){
-            if (err){
-                done(false);
-            } else {
-                done();
-            }
         });
     });
 };
